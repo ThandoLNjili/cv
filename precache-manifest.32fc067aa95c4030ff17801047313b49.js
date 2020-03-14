8c91f74ffb9e63df68b9ecbd51c906317d5cb572
Thandolwethu NJILI <tnjili@c3r2s3.wethinkcode.co.za>
2020-03-14 21:12:30 +0200

    Updates

---
+++ precache-manifest.32fc067aa95c4030ff17801047313b49.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397c39045fc7edfb742275de00a6b6e9",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "bf51c7a6edbe9d4f2167",
+    "url": "/cv/static/css/2.b2f0a3dd.chunk.css"
+  },
+  {
+    "revision": "f3a1a10fd8a8b4f61998",
+    "url": "/cv/static/css/main.cf525a46.chunk.css"
+  },
+  {
+    "revision": "bf51c7a6edbe9d4f2167",
+    "url": "/cv/static/js/2.f2272df6.chunk.js"
+  },
+  {
+    "revision": "f3a1a10fd8a8b4f61998",
+    "url": "/cv/static/js/main.12c4c607.chunk.js"
+  },
+  {
+    "revision": "33eb0f565a8b7bacd371",
+    "url": "/cv/static/js/runtime~main.9c234b58.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/cv/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/cv/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/cv/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/cv/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/cv/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d7b4a531bfd303d92b7953a944698ac2",
+    "url": "/cv/static/media/profile-picture.d7b4a531.jpg"
+  }
+]);
